8c6fbca6a017a1e156058413a3cb05f7468e0645
Shahali Bogdadi <bogdadishahali@gmail.com>
2021-12-06 16:33:08 +0600

    declared specific type for profileItem component

--- src/shared/Component/ProfileItem/index.tsx
+++ src/shared/Component/ProfileItem/index.tsx
@@ -2,8 +2,13 @@ import React from 'react';
 import { Grid, List, ListItem, ListItemAvatar, ListItemIcon, ListItemText, Divider } from '@mui/material';
 import classes from './profileItem.module.css'
 
+interface DataProps {
+    icon: React.ReactElement,
+    data: string,
+    title: string,
+}
 
-const ProfileItem = ({ icon, data, title }: any): JSX.Element => {
+const ProfileItem = ({ icon, data, title }: DataProps) => {
     return (
         <Grid item xs={12} sm={6}>
             <List className={classes.positionProfile}>
