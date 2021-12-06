79a76b3430b11dc92f32549faeb2e7a3f4a003dc
Shahali Bogdadi <bogdadishahali@gmail.com>
2021-12-06 16:42:35 +0600

    profileImage component created

--- src/modules/Application/Profile/index.tsx
+++ src/modules/Application/Profile/index.tsx
@@ -1,29 +1,16 @@
 import React from 'react';
-import { Container, Grid, Paper, Box, Avatar, Divider } from '@mui/material';
+import { Container, Grid, Paper } from '@mui/material';
 import { LensBlur, Email, Room, Phone, Language, Work } from '@mui/icons-material';
 
 import ProfileItem from '../../../shared/Component/ProfileItem';
+import ProfileImage from '../../../shared/Component/ProfileImage';
 
 const Profile = () => {
     return (
         <Container maxWidth={'md'}>
             <Grid container spacing={3}>
                 <Grid item md={12} xs={12}>
-                    <Paper sx={{
-                        marginTop: 4,
-                        display: "flex",
-                        flexDirection: "column",
-                        alignItems: "center",
-                        width: "100%"
-                    }}>
-                        <Box p={5}>
-                            <Avatar
-                                alt="Shah Ali Bogdadi"
-                                src="/static/images/avatar/1.jpg"
-                                sx={{ width: 128, height: 128 }}
-                            />
-                        </Box>
-                    </Paper>
+                    <ProfileImage />
                     <Paper sx={{
                         marginTop: 2,
                         width: "100%"
@@ -36,7 +23,7 @@ const Profile = () => {
                             <ProfileItem icon={<Room />} title='Present Address' data='Dhaka' />
                             <ProfileItem icon={<Room />} title='Permanent Address' data='Narayangonj, Dhaka' />
                             <ProfileItem icon={<Phone />} title='Mobile No' data='01234567890' />
-                            <ProfileItem icon={<Phone />} title='Mobile No' data='01234567890' />
+                            <ProfileItem icon={<Phone />} title='Alternate Mobile No' data='017000000' />
                             <ProfileItem icon={<Language />} title='Country' data='Bangladesh' />
                             <ProfileItem icon={<Work />} title='Occupation' data='Software Engineer (Frontend)' />
                         </Grid>
---
+++ src/shared/Component/ProfileImage/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Paper, Box, Avatar } from '@mui/material';
+
+const ProfileImage = () => {
+    return (
+        <Paper sx={{
+            marginTop: 4,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            width: "100%"
+        }}>
+            <Box p={5}>
+                <Avatar
+                    alt="Shah Ali Bogdadi"
+                    src="/static/images/avatar/1.jpg"
+                    sx={{ width: 128, height: 128 }}
+                />
+            </Box>
+        </Paper>
+    )
+}
+
+export default ProfileImage;
